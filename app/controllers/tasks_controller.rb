8907007ea183f55736697eae617e89a90f34c4ca
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-02-20 13:58:51 +0100

    working no style yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,71 @@
 class TasksController < ApplicationController
 
-  # 1. index() method (with .all) to fetch all @tasks
+  # READ (1) -- fetch all @tasks
   # Make all tasks a variable with @ so it can be used in View
   def index
     @tasks = Task.all
   end
 
-  # 2. show() method (with .find(id)) to show one task
+  # READ (2) -- show one task
   # Make readable task variable with @ so it can be used in View
   def show
-    id = params[:id]
-    @task = Task.find(id)
+    find_task()
   end
 
-  # 3. new() method (with .new) to create a task
+  # CREATE (1) -- create a empty task for form initialization
   # Make new task a variable with @ so it can be used in View as "emtpy recepticle"
   def new
     @task = Task.new
   end
 
-  # 4. create() method to use form input to create a task to display on /tasks
-  # No view required since it's post :)
+  # CREATE (2) -- create task instance w/form input
+  # No need for app/views/restaurants/update.html.erb
   def create
     # 1. Fill in the empty Task generated with .new with the parameters posted from the form (params)
-    # @task = Task.new(params[:task]) does not work, task_params must be called instead to get params!
+    # Task_params must be called to get params to create a new Task instance
     @task = Task.new(task_params)
-    # 2. Save the new Task instance
     @task.save
     # 3. Add redirect to go back to tasks page with redirect_to method (redirect_to xxx_path(@instance))
     redirect_to tasks_path
   end
 
+  # UPDATE (1) -- find existing task instance
+  def edit
+    find_task()
+  end
+
+  # UPDATE (2) -- edit the existing task instance w/form input
+  def update
+    find_task()
+    @task.update(task_params)
+    # No need for app/views/tasks/update.html.erb
+    # Task_params must be called instead to get params; are the params given in form
+    # Redirect to task (id) page
+    redirect_to task_path
+  end
+
+  # DELETE -- destroy task instance w/form input
+  def destroy
+    find_task()
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    # Redirect to tasks page, add status: :see_other to prevent 303 error
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
-  # 4. for create() method - to allow use of the new task instance parameters to use on /tasks
+  # MAKING TASK PARAMETERS USABLE FOR METHODS ABOVE (create & update)
+  # (so to allow use of the new task instance parameters to create/edit a task instance)
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
+  # For use in multiple methods; get @task
+  def find_task
+    id = params[:id]
+    @task = Task.find(id)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,31 @@
 Rails.application.routes.draw do
 
+  # view only required for get
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # 1. GET all (.all) - http://localhost:3000/tasks/
+  # READ (1) -- GET all to give list (index) - http://localhost:3000/tasks/
   get "/tasks", to: "tasks#index"
 
-  # 3. GET form to add (new) task - http://localhost:3000/tasks/new
+  # CREATE (1) -- GET form to add new task - http://localhost:3000/tasks/new
   get "tasks/new", to: "tasks#new"
 
-  # 4. POST form input to (create task for) /tasks page - http://localhost:3000/tasks/
+  # CREATE (2) -- POST form input to (create task for) /tasks page - http://localhost:3000/tasks/
   # -- no view required since it's post (also for patch & destroy!)
   post "/tasks", to: "tasks#create"
 
-  # 2. GET one (.find(id)) - http://localhost:3000/tasks/1 - dynamic URL
+  # READ (2) -- GET one (.find(id)) - http://localhost:3000/tasks/1 - dynamic URL
   # , as: task is added to get prefix for this one as well :)
   get "tasks/:id", to: "tasks#show", as: :task
+
+  # EDIT (1) -- GET form to edit task - http://localhost:3000/tasks/:id/edit
+  get "tasks/:id/edit", to: "tasks#edit"
+
+  # EDIT (2) -- PATCH form to update task instance - http://localhost:3000/tasks/:id
+  patch "tasks/:id", to: "tasks#update"
+
+  # REMOVE -- DELETE task w/id to destroy task instance - http://localhost:3000/restaurants/1
+  # Not their own view, delete option in task instance page 
+  delete "tasks/:id", to: "tasks#destroy"
 
 end
